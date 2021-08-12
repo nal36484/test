87a56d1c65f77b457637128b4c514a9b8cda446f
nal36484 <83834922+nal36484@users.noreply.github.com>
2021-08-12 19:03:34 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,9 +10,9 @@
 
 int main()
 {
-	constexpr size_t sz = 10;   // размер первого массива
+	constexpr size_t sz = 10;   // СЂР°Р·РјРµСЂ РїРµСЂРІРѕРіРѕ РјР°СЃСЃРёРІР°
 	const double pi = std::acos(-1);    
-	std::vector<PCurves> curves_vec;  // массив указателей на объекты базового класса
+	std::vector<PCurves> curves_vec;  // РјР°СЃСЃРёРІ СѓРєР°Р·Р°С‚РµР»РµР№ РЅР° РѕР±СЉРµРєС‚С‹ Р±Р°Р·РѕРІРѕРіРѕ РєР»Р°СЃСЃР°
 	{
 		Curves_generator s_gen;  
 		s_gen.set_diapason(-10, 10);    
@@ -20,33 +20,33 @@ int main()
 			curves_vec.emplace_back(s_gen.random_curve());   
 	}
 
-	using PCircles = std::shared_ptr<Circles3D>;  // псевдоним типа - умный указатель на Circles3D
+	using PCircles = std::shared_ptr<Circles3D>;  // РїСЃРµРІРґРѕРЅРёРј С‚РёРїР° - СѓРјРЅС‹Р№ СѓРєР°Р·Р°С‚РµР»СЊ РЅР° Circles3D
 
-	std::vector<PCircles> circles_vec;    // массив указателей на Circles3D из первого контейнера
+	std::vector<PCircles> circles_vec;    // РјР°СЃСЃРёРІ СѓРєР°Р·Р°С‚РµР»РµР№ РЅР° Circles3D РёР· РїРµСЂРІРѕРіРѕ РєРѕРЅС‚РµР№РЅРµСЂР°
 
-	double radius_sum{};    // сумма радиусов Circles3D
+	double radius_sum{};    // СЃСѓРјРјР° СЂР°РґРёСѓСЃРѕРІ Circles3D
 	for (auto& pc : curves_vec)
 	{
-		const std::type_info& ti{ typeid(*pc) };    // информация о типе объекта, на который указывает pc из первого контейнера
+		const std::type_info& ti{ typeid(*pc) };    // РёРЅС„РѕСЂРјР°С†РёСЏ Рѕ С‚РёРїРµ РѕР±СЉРµРєС‚Р°, РЅР° РєРѕС‚РѕСЂС‹Р№ СѓРєР°Р·С‹РІР°РµС‚ pc РёР· РїРµСЂРІРѕРіРѕ РєРѕРЅС‚РµР№РЅРµСЂР°
 
-		std::cout << ti.name() << ' '   // выводим имя типа
-			<< pc->curve_point(pi / 4) << ' '   // точку кривой, заданную параметром
-			<< pc->curve_vector(pi / 4) << std::endl;   // вектор(первая производная - касательная) в предыдущей точке
+		std::cout << ti.name() << ' '   // РІС‹РІРѕРґРёРј РёРјСЏ С‚РёРїР°
+			<< pc->curve_point(pi / 4) << ' '   // С‚РѕС‡РєСѓ РєСЂРёРІРѕР№, Р·Р°РґР°РЅРЅСѓСЋ РїР°СЂР°РјРµС‚СЂРѕРј
+			<< pc->curve_vector(pi / 4) << std::endl;   // РІРµРєС‚РѕСЂ(РїРµСЂРІР°СЏ РїСЂРѕРёР·РІРѕРґРЅР°СЏ - РєР°СЃР°С‚РµР»СЊРЅР°СЏ) РІ РїСЂРµРґС‹РґСѓС‰РµР№ С‚РѕС‡РєРµ
 
-		if (ti.hash_code() == typeid(Circles3D).hash_code()) // если текущий объект - указатель на Circles3D
+		if (ti.hash_code() == typeid(Circles3D).hash_code()) // РµСЃР»Рё С‚РµРєСѓС‰РёР№ РѕР±СЉРµРєС‚ - СѓРєР°Р·Р°С‚РµР»СЊ РЅР° Circles3D
 		{
-			circles_vec.emplace_back(std::dynamic_pointer_cast<Circles3D>(pc));   // кастим его к указателю на Circles3D
-			radius_sum += circles_vec.back()->getRadius();  // плюсуем радиус
-			std::cout << "to another vector -->\n"; // debug (можно удалить эту строку)
+			circles_vec.emplace_back(std::dynamic_pointer_cast<Circles3D>(pc));   // РєР°СЃС‚РёРј РµРіРѕ Рє СѓРєР°Р·Р°С‚РµР»СЋ РЅР° Circles3D
+			radius_sum += circles_vec.back()->getRadius();  // РїР»СЋСЃСѓРµРј СЂР°РґРёСѓСЃ
+			std::cout << "to another vector -->\n"; // РїСЂРѕРІРµСЂРєР°
 		}
 		std::cout << std::endl;
 	}
-	std::sort(circles_vec.begin(), circles_vec.end(), [](const PCircles& lh, const PCircles& rh)	// сортируем по радиусу
+	std::sort(circles_vec.begin(), circles_vec.end(), [](const PCircles& lh, const PCircles& rh)	// СЃРѕСЂС‚РёСЂСѓРµРј РїРѕ СЂР°РґРёСѓСЃСѓ
 		{
 			return lh->getRadius() < rh->getRadius();
 		});
-	for (auto& pc : circles_vec)	// демонстрируем отсортированное
+	for (auto& pc : circles_vec)	// РґРµРјРѕРЅСЃС‚СЂРёСЂСѓРµРј РѕС‚СЃРѕСЂС‚РёСЂРѕРІР°РЅРЅРѕРµ
 		std::cout << pc->getRadius() << std::endl;
-	std::cout << "sum = " << radius_sum << std::endl;	// и сумму радиусов всех Circles3D
+	std::cout << "sum = " << radius_sum << std::endl;	// Рё СЃСѓРјРјСѓ СЂР°РґРёСѓСЃРѕРІ РІСЃРµС… Circles3D
 }
 
